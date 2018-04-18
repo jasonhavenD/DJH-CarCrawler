9aeb88037be5afbbcfb6e8e4e7f334952cfe44b3
JasonhavenDai <951771966@qq.com>
2018-04-18 23:06:53 +0800

    update code

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # DJH-CarCrawler
 ---
-[汽车产经网](http://www.autoreport.cn/newslist/a1169/)
-[汽车之家新闻](https://www.autohome.com.cn/news/?p=s#liststart)
-[中国汽车新闻网](http://www.chinaautonews.com.cn/list-6-1.html)
+###### Done
+- [中国汽车新闻网](http://www.chinaautonews.com.cn/list-6-1.html)
+
+###### Not Yet
+- [汽车产经网](http://www.autoreport.cn/newslist/a1169/)
+- [汽车之家新闻](https://www.autohome.com.cn/news/?p=s#liststart)
 
--- src/util/chinaautonews_crawler.py
+++ src/util/chinaautonews_crawler.py
@@ -49,17 +49,24 @@ class ChinaautonewsCrawler():
 
 	def crawl_page(self, url):
 		req = request.Request(url=url, headers=headers)
-		resp = request.urlopen(req)
+		resp = request.urlopen(req, timeout=5)
 		if resp.status != 200:
 			logger.error('url open error. url = {}'.format(url))
 		html_doc = self.pre_process_html_doc(resp.read(), url, resp)
 		soup = BeautifulSoup(html_doc, "lxml")
+
+		title = soup.select_one("h1")
+		if title != None and title.text.strip() == "版权声明":
+			logger.info(("版权声明"))
+			return None, None
+
 		clean_html = tool.Clean_html()
 		title = soup.select_one('h1').string
 		text_tag = soup.find('div', attrs={'class': 'detail-content'})
 		text = ""
 		for p in text_tag.find_all('p'):
 			text += p.text
+		self.save_url(url)
 		return title, clean_html.clean(text)
 
 	def save_text(self, title, text, href):
@@ -69,25 +76,30 @@ class ChinaautonewsCrawler():
 			f.write(text)
 			logger.info("text of {} has all saved as {}!".format(href, file))
 
+	def save_url(self, url):
+		file = '../../result/chinaautonews/urls.lst'
+		with codecs.open(file, 'a', encoding='utf-8') as f:
+			f.write(url)
+			f.write("\n")
+			logger.info("url {} has been saved !".format(url))
+
 	def pre_process_html_doc(self, html_doc, url, resp):
 		try:
 			if resp.getheader('Content-Encoding') == 'gzip':
 				buf = io.BytesIO(html_doc)
 				gf = gzip.GzipFile(fileobj=buf)
 				html_doc = gf.read()
-			return html_doc
+			return html_doc.decode('utf-8')
 		except Exception as e:
 			logger.error("{}: url = {}".format(e, url))
 
 	def begin(self, start_page=0, end_page=10):
-		start_url = self.domain + "list-13-" + str(start_page) + ".html"
-		url = start_url
 		next_page = start_page
 		begin = datetime.datetime.now()
 		number = 1
 		while (next_page < end_page):
 			next_page += 1
-			url = self.domain + "list-13-" + str(next_page) + ".html"
+			url = self.domain + self.url_middle + str(next_page) + ".html"
 
 			# 使用IP池
 			# proxy = tool.get_random_proxy()
@@ -97,22 +109,32 @@ class ChinaautonewsCrawler():
 			# request.install_opener(opener)
 
 			req = request.Request(url=url, headers=headers)
-			resp = request.urlopen(req)
-			time.sleep(1)
+			resp = request.urlopen(req, timeout=5)
+
+			time.sleep(3)
+
 			if resp.status != 200:
 				logger.error('url open error. url = {}'.format(start_url))
 			html_doc = self.pre_process_html_doc(resp.read(), url, resp)
 			soup = BeautifulSoup(html_doc, "lxml")
+
 			li_tags = soup.find("div", attrs={"class": "lside_nr"}).find_all("li")
+
 			for li in li_tags:
 				href = li.select_one('a').get('href')
 				if not href.startswith('http://www.chinaautonews.com.cn/show'):
 					continue
 				logger.info("crawling page {} url = {}".format(next_page, href))
+				# time.sleep(1)
 				title, text = self.crawl_page(href)
+
+				if title == None or text == None:
+					logger.info("filter url = {}".format(url))
+					continue
+
 				name = str(number) + "-" + re.sub(re.compile(r'["/ ]'), '_', title)[:2]
 				number += 1
-				self.save_text(name, text, href)
+			# self.save_text(name, text, href)
 		end = datetime.datetime.now()
 		print('finished in ' + str((end - begin).seconds) + ' s!')
 
@@ -143,16 +165,16 @@ if __name__ == '__main__':
 	inter_crawler = ChinaautonewsCrawler("inter", url_middles_dict["inter"])
 	domestic_crawler = ChinaautonewsCrawler("domestic", url_middles_dict["domestic"])
 
-	news_crawler.begin(0,130)
+	# news_crawler.begin(0, 130)
 
-	# thread_news = MyThread(1, "news_crawler", news_crawler, 0, 130)
-	# thread_inter = MyThread(2, "inter_crawler", inter_crawler, 0, 45)
-	# thread_domestic = MyThread(3, "domestic_crawler", domestic_crawler, 0, 60)
-	#
-	# thread_news.start()
-	# thread_inter.start()
-	# thread_domestic.start()
-	#
-	# thread_news.join()
-	# thread_inter.join()
-	# thread_domestic.join()
+	thread_news = MyThread(1, "news_crawler", news_crawler, 0, 130)
+	thread_inter = MyThread(2, "inter_crawler", inter_crawler, 0, 45)
+	thread_domestic = MyThread(3, "domestic_crawler", domestic_crawler, 0, 60)
+
+	thread_news.start()
+	thread_inter.start()
+	thread_domestic.start()
+
+	thread_news.join()
+	thread_inter.join()
+	thread_domestic.join()
